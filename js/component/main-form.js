d9993830b068065555e5c365b8a1d5829ef5cb32
Lucas Kahl <lucaschangkahl@gmail.com>
2018-10-01 17:48:27 -0300

    Adicioanndo comandos JS

--- js/component/main-form.js
+++ js/component/main-form.js
@@ -1,2 +1,21 @@
 //var isEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+const mainTableEl = document.querySelector('.main-table');
+const tableBodyEl = document.querySelector('#table-users');
+var trEl = document.createElement("tr");  
+
+function getUsers() {
+
+  let users = fetch('https://private-21e8de-rafaellucio.apiary-mock.com/users');
+
+  users
+  .then(data => data.json())
+  .then(data => data.map(inf => {
+    console.log(inf.name);
+  
+  }));
+
+}
+
+getUsers();
+
